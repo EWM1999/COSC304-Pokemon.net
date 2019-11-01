1a3a001b5b22f0f0574b73ab0fd53de805bd3d63
Emily <medema.emily@gmail.com>
2019-11-01 12:38:58 -0700

    [Update]: myedits to Lab7

---
+++ Lab7/include/db_credentials.php
@@ -0,0 +1,7 @@
+<?php
+	$username = "emedema";
+	$password = "91175448";
+	$database = "db_" . $username;
+	$server = "sql04.ok.ubc.ca";
+	$connectionInfo = array( "Database"=>$database, "UID"=>$username, "PWD"=>$password, "CharacterSet" => "UTF-8");
+?>
