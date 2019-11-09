f5b49c1a120ee4dc38be81dbf67557d52bc7b26f
Emily <medema.emily@gmail.com>
2019-11-08 16:19:15 -0800

    Initial Changes
    Changed name and a couple other factors.

--- Lab7/listorder.php
+++ Lab7/listorder.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>YOUR NAME Grocery Order List</title>
+<title>Pokemon.net - Order List</title>
 </head>
 <body>
 
@@ -32,8 +32,25 @@ Useful code for formatting currency:
 
 
 /** Close connection **/
-?>
+	$username = "fill-in";
+	$password = "fill-in";
+	$database = "WorksOn";
+	$server = "sql04.ok.ubc.ca";
+	$connectionInfo = array( "Database"=>$database, "UID"=>$username, "PWD"=>$password, "CharacterSet" => "UTF-8");
 
+	$con = sqlsrv_connect($server, $connectionInfo);
+	if( $con === false ) {
+		die( print_r( sqlsrv_errors(), true));
+	}
+
+	$sql = "SELECT ename,salary FROM emp";
+	$results = sqlsrv_query($con, $sql, array());
+	echo("<table><tr><th>Name</th><th>Salary</th></tr>");
+	while ($row = sqlsrv_fetch_array( $results, SQLSRV_FETCH_ASSOC)) {
+		echo("<tr><td>" . $row['ename'] . "</td><td>" . $row['salary'] . "</td></tr>");
+	}
+	echo("</table>");
+?>
 </body>
 </html>
 
