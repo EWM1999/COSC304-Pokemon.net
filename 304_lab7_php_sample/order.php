b6bdabdb7e4b3366c33f59e236236168905b6d95
Kathryn Lecha <43145003+kzlecha@users.noreply.github.com>
2019-11-14 00:33:22 -0800

    Update order.php

--- 304_lab7_php_sample/order.php
+++ 304_lab7_php_sample/order.php
@@ -2,12 +2,13 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>YOUR NAME Grocery Order Processing</title>
+<title>Pokeshop Order Processing</title>
 </head>
 <body>
 
 <?php
 include 'include/db_credentials.php';
+
 /** Get customer id **/
 $custId = null;
 if(isset($_GET['customerId'])){
@@ -17,6 +18,8 @@ session_start();
 $productList = null;
 if (isset($_SESSION['productList'])){
 	$productList = $_SESSION['productList'];
+}else{
+	die("<p>There are no products in your shopping cart :( </p>");
 }
 
 /**
@@ -25,10 +28,31 @@ Determine if there are products in the shopping cart
 If either are not true, display an error message
 **/
 
+// valid customer id?
+// maybe if its an integer? Because the ddl wants one?
+if(!is_int($custId)){
+	die("<p>You did not enter a valid customer id :( </p>");
+}
+// maybe if its in the customer list
+
+include 'include/db_credentials.php';
+$con = sqlsrv_connect($server, $connectionInfo);
+
+
+$sql = "SELECT * FROM customer WHERE customerId = ?;";
+$result = sqlsrv_query($con, $sql, array($custId));
+if(!$result){
+	die("<p>You are not a valid customer :( </p>");
+}
+
+
+
 /** Make connection and validate **/
 
-/** Save order information to database**/
+// oh fuck i did that earlier
+$row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC);
 
+/** Save order information to database**/
 
 	/**
 	// Use retrieval of auto-generated keys.
@@ -39,13 +63,63 @@ If either are not true, display an error message
 	}
 	$orderId = sqlsrv_get_field($pstmt,0);
 	**/
+$customer_name = $row['firstName']." ".$row['lastName'];
+
+$orderDate = date('Y-m-d H:i:s');
+
+// ok Ramon
+$sql = "INSERT INTO ordersummary (customerId, totalAmount, orderDate) OUTPUT INSERTED.orderId VALUES(?, 0, ?);";
+$result = sqlsrv_query($con, $sql2, array($custId, $orderDate));
+
+if(!$result){
+	echo("idk the query didn't insert or something. idk how to fix that");
+}
+
+$orderId = sqlsrv_get_field($pstmt,0);
+
+echo("<h1>Your Order Summary</h1>");
+echo("<table>");
+echo("<tr><th>Product Id</th><th>Product Name</th><th>Quantity</th><th>Price</th><th>Subtotal</th></tr>");
 
 /** Insert each item into OrderedProduct table using OrderId from previous INSERT **/
 
+$total = 0;
+foreach($productList as $id => $product){
+	// maybe someone can check this?
+	$product_id = intval($product['id'], 10);
+	$product_name = $product['name'];
+	$product_price = floatval($product['price']);
+	$product_quantity = $product['quantity'];
+
+	echo("<tr>");
+	echo("<td>".$product_id."</td>");
+	echo("<td>".$product_name."</td>");
+
+	echo("<td align=\"center\">".$product_quantity."</td>");
+	echo("<td align=\"right\">$". number_format($product_price, 2)."</td>");
+
+	$subtotal = $product_price * $product_quantity;
+	echo("<td align=\"right\">$". number_format($subtotal, 2)."</td></tr>");
+	echo("</tr>");
+
+	$total = $total + $product_price * $product_quantity;
+	$sql = "INSERT INTO OrderProduct (orderId, productId, quantity, price) VALUES(?, ?, ?, ?)";
+	$result = sqlsrv_query($con, $sql, array($orderId, $product_id, $product_quantity, $product_price));
+	if(!$result){
+		echo("the ordered product didn't insert so i think thats whats wrong but idk maybe i forgot a semicolon");
+	}
+}
+
 /** Update total amount for order record **/
 
-/** For each entry in the productList is an array with key values: id, name, quantity, price **/
+$sql = "UPDATE ordersummary SET totalAmount = ? WHERE orderId = ?;";
+$result = sqlsrv_query($con, $sql, array($total, $orderId));
 
+if(!$result){
+	echo("the update failed and i think i need a nap");
+}
+
+/** For each entry in the productList is an array with key values: id, name, quantity, price **/
 /**
 	foreach ($productList as $id => $prod) {
 		\\$prod['id'], $prod['name'], $prod['quantity'], $prod['price']
@@ -53,9 +127,23 @@ If either are not true, display an error message
 	}
 **/
 
+// wait isn;t that what im supposed to do before the update?
+// why would you do that after the update?
+// that makes no sense
+
+// im not going to do it
+
 /** Print out order summary **/
 
+echo("Order Complete :). Your pokemon will be wired to you shortly<br>");
+echo("Your order reference is: ".$orderId."<br");
+echo("We are shipping to Pokemon Trainer ".$customer_name." with id ".$custId."</br>");
+echo("friendly reminder that animal fighting is a crime :)");
+
+echo("<h2><a href=\"shop.html\">Return to collecting 'em all :)</a></h2>");
+
 /** Clear session/cart **/
+$_SESSION['productList'] = NULL;
 ?>
 </body>
 </html>
