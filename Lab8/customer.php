c094eb67697efb0bc1f6fa7c33b4290efee84cec
Emily <medema.emily@gmail.com>
2019-11-20 14:25:37 -0800

    Update customer.php
    customer.php should work

--- Lab8/customer.php
+++ Lab8/customer.php
@@ -99,16 +99,25 @@
     include 'auth.php';
     include 'header.php';
     include 'include/db_credentials.php';
+	$con = sqlsrv_connect($server, $connectionInfo);
+	if( $con === false ) {
+		die( print_r( sqlsrv_errors(), true));
+	}
 ?>
 
 <?php
 $user = $_SESSION['authenticatedUser'];
     
 // TODO: Print Customer information
+echo "<img src='https://i.imgur.com/LH65A0E.png' border='0'>";
 
-$sql = "SELECT customerId, firstName, lastName, email, phonenum, address, city, state, postalCode, country, userid from customer where id = ?";
-
-
+$sql = "SELECT customerId, firstName, lastName, email, phonenum, address, city, state, postalCode, country, userid from customer where userid = ?";
+$results = sqlsrv_prepare($con, $sql, array($user));
+if($rst = sqlsrv_fetch_array( $results, SQLSRV_FETCH_ASSOC)){
+	echo("<table border = \"1\"> <tr><td>Id</td><td>".$rst['customerId']."</td></tr><tr><td>First Name</td><td>".$rst['firstName']."</td></tr><tr><td>Last Name</td><td>".$rst['lastName']."</td></tr><tr><td>Email</td><td>".$rst['email']."<tr><td>Phone</td><td>".$rst['phone']."</td></tr><tr><td>Address</td><td>".$rst['aadress']."</td></tr><tr><td>City</td><td>".$rst['city']."</td></tr><tr><td>State</td><td>".$rst['state']."</td></tr><tr><td>Postal Code</td><td>".$rst['postalCode']."</td></tr><tr><td>Country</td><td>".$rst['country']."</td></tr><tr><td>User Id</td><td>".$rst['userid']."</td></tr></table>")
+}
+sqlsrv_free_stmt($results);
+sqlsrv_close($con);
 // Make sure to close connection
 ?>
 </body>
