851a09d3a03f18311b761054cb0878b544670f5a
Emily <medema.emily@gmail.com>
2019-11-20 13:04:13 -0800

    Update
    Lab 8 starter code, with changed index. Kat edits on listprod and order

--- Lab7/checkout.php
+++ Lab7/checkout.php
@@ -1,17 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Grocery CheckOut Line</title>
+<title>Pokémon CheckOut Line</title>
 </head>
 <body>
 
-<h1>Enter your customer id to complete the transaction:</h1>
+<h1>Enter your customer id and password to complete the transaction:</h1>
 
-<form method="get" action="order.php">
-<input type="text" name="customerId" size="50">
-<input type="submit" value="Submit"><input type="reset" value="Reset">
+<form method="POST" action="order.php">
+<input type="text" name="customerId" size="50" required>
+<input type="password" name="password" size="30" required>
+<input type="submit" value="Submit">
+<input type="reset" value="Reset">
 </form>
 
 </body>
 </html>
-
