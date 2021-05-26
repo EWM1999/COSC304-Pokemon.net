bcd3f88bf4566f72effa596863ea0d23e2ddab94
kzlecha <kzlecha@gmail.com>
2021-05-26 15:18:58 -0700

    remove ziped lab

--- Lab8/lab8/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-	// Remove the user from the session to log them out	
-    session_start();
-	$_SESSION['authenticatedUser'] = null;
-	$_SESSION['admin'] == 0;
-	$_SESSION['productList'] = null;
-	header('Location: index.php');	
-?>
-
