d38e08f75b2dd7ed4d9116cb7739eada63591a81
kzlecha <kzlecha@gmail.com>
2021-05-26 15:34:11 -0700

    remove midpoint milestone code

--- Lab7/include/db_credentials.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$username = "emedema";
-	$password = "91175448";
-	$database = "db_" . $username;
-	$server = "sql04.ok.ubc.ca";
-	$connectionInfo = array( "Database"=>$database, "UID"=>$username, "PWD"=>$password, "CharacterSet" => "UTF-8");
-?>
